11dfb9db83434303540878a0612cd1c45c82b281
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-15 12:20:30 +0100

    Update grades.js

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,18 @@
+let votiMario = {
+    voti: [],
 
+    aggiungi(voto) {
+        this.voti.push(voto)
+    },
+
+    mostraMedia() {
+        return this.voti.reduce((acc, curr) => acc + curr) / this.voti.length
+    }
+}
+
+votiMario.aggiungi(10)
+votiMario.aggiungi(8)
+votiMario.aggiungi(6)
+
+console.log(votiMario.voti)
+console.log(votiMario.mostraMedia())
