bb4e291e407a10b6579e924bf6f72242b9b5b776
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-15 12:21:43 +0100

    Update words.js

--- array/words.js
+++ array/words.js
@@ -1,1 +1,26 @@
+let parole = {
+    parole: [],
+    aggiungi(parola){
+        this.parole.push(parola)
+    },
+    paroleDritte() {
+        return this.parole.join(", ")
+    },
+    paroleAlContrario() {
+        return this.parole.reduce((acc, curr) => {
+            acc.unshift(curr);
+            return acc;
+        }, []).join(", ")
+    },
+    paroleAlContrario2() {
+        return this.parole.reduce((acc, curr) => curr + ", " + acc)
+    }
+}
 
+parole.aggiungi("parola1")
+parole.aggiungi("parola2")
+parole.aggiungi("parola3")
+
+console.log(parole.parole)
+console.log(parole.paroleDritte())
+console.log(parole.paroleAlContrario2())
