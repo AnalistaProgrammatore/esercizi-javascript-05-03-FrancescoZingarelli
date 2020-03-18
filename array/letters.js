b8e4885d37dc6ae935d7ffdda3753743034e54bf
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-18 19:01:30 +0100

    Update letters.js

--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,24 @@
+class Parola {
+    constructor() {
+        this.lettere = []
+    }
 
+    aggiungi(lettera) {
+        this.lettere.push(lettera);
+    }
+
+    unisci() {
+        return this.lettere.join("")
+    }
+}
+
+
+
+let nuovaParola = new Parola;
+
+nuovaParola.aggiungi("c");
+nuovaParola.aggiungi("a");
+nuovaParola.aggiungi("n");
+nuovaParola.aggiungi("e");
+
+console.log(nuovaParola.unisci())
