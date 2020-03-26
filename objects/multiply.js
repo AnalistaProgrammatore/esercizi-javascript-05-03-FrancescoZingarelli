473d0d87e3f02fdfa39256dfef0ff31e30d70cf9
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-26 18:16:10 +0100

    Update multiply.js

--- objects/multiply.js
+++ objects/multiply.js
@@ -1,1 +1,35 @@
+// NE HO FATTE DUE VERSIONI:
 
+
+
+// VERSIONE PRIMA COPIANDO TUTTO E POI MODIFICANDO LE PROPRIETA' NUMERICHE
+
+function multiplyNumeric({...properties}, factor) {
+    let newObj = {...properties};
+    Object.keys(newObj).filter(key => typeof newObj[key] === "number").map(key => newObj[key] *= factor)
+    return newObj;
+}
+
+
+
+// VERSIONE MOLTIPLICANDO DURANTE LA COPIA
+
+function multiplyNumeric2(obj, factor) {
+    return Object.keys(obj).reduce((newObj, key) => {
+            (typeof obj[key] === "number") ? newObj[key] = obj[key] * factor : newObj[key] = obj[key]
+            return newObj
+        }, {})
+}
+
+
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+}
+
+const newMenu = multiplyNumeric(menu, 2)
+
+console.log(menu);
+console.log(newMenu);
