6d2a6677e01799d0ddb1f34c422dcf11b53b81a8
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-26 17:14:52 +0100

    Update salaries.js

--- objects/salaries.js
+++ objects/salaries.js
@@ -1,1 +1,11 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
 
+function sumSalaries(obj) {
+    return Object.keys(obj).map(key => obj[key]).reduce((sum, curr) => sum + curr)
+}
+
+console.log(sumSalaries(salaries));
