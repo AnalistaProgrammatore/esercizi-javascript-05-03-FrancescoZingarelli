6b7db48dee5a716260c70525db6bbb68ba437b15
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-26 16:48:27 +0100

    Update hello.js

--- objects/hello.js
+++ objects/hello.js
@@ -1,1 +1,15 @@
+let user = {};
 
+user.name = "Mario";
+
+Object.defineProperty(user, 'surname', {
+    value: "Rossi",
+    writable: false,
+    configurable: false
+});
+
+user.name = "Marco";
+
+delete user.name;
+
+console.log(Object.getOwnPropertyDescriptors(user))
