11b450b19862325ed23ef3c439e95895f43f1dd7
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-26 18:56:46 +0100

    Update calculatorConstructor.js

--- objects/calculatorConstructor.js
+++ objects/calculatorConstructor.js
@@ -1,1 +1,36 @@
+function Calculator(values) {
+    this.values = values;
 
+    this.sum = function() {
+        return this.values.reduce((acc, cur) => acc + cur);
+    }
+
+    this.sub = function() {
+        return this.values.reduce((acc, cur) => acc - cur);
+    }
+
+    this.mul = function() {
+        return this.values.reduce((acc, cur) => acc * cur);
+    }
+
+    this.div = function() {
+        let zero = false;
+        return this.values.reduce((acc, cur) => {
+                if (!zero) {
+                    if (cur !== 0) {
+                        return acc / cur;
+                    }
+                    zero = true;
+                    return "non puoi dividere per zero!"
+                }
+                return acc
+            });
+    }
+}
+
+let calculator = new Calculator([6, 3, 2]);
+
+console.log( calculator.sum() )
+console.log( calculator.sub() )
+console.log( calculator.mul() )
+console.log( calculator.div() )
